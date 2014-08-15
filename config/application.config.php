ccd666a2aa90731c5434c30efae86c498879da4c
Wernedres <wernedres@gmail.com>
2014-08-15 10:59:18 -0400

    [ADJUST] Listando Registro
    Signed-off-by: Wernedres <wernedres@gmail.com>

--- config/application.config.php
+++ config/application.config.php
@@ -1,4 +1,5 @@
 <?php
+
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
@@ -6,20 +7,16 @@ return array(
         'DoctrineORMModule',
         'Livraria'
     ),
-
-  
     'module_listener_options' => array(
-        'config_glob_paths' =>array(
+        'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
-            'module_paths' => array(
+        'module_paths' => array(
             './module',
             './vendor',
-   ),
+        ),
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
     ),
-
-   
 );
--- module/Livraria/src/Livraria/Entity/Categoria.php
+++ module/Livraria/src/Livraria/Entity/Categoria.php
@@ -1,55 +1,54 @@
 <?php
 
-
 namespace Livraria\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
- * @ORM\table (name= "categorias")
- * @ORM\Entity(repositoryClass="Livraria\Entity\CategoriaRepository")
+ * @ORM\Table(name="categorias")
  */
-
 class Categoria {
+
     /**
- * @ORM\id
- * @ORM\column (type= "integer")
- * @ORM\GeneratedValue
- */
-protected $id;
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     */
+    protected $id;
 
-    /** 
- * 
- * @ORM\column (type= "text")
- * @var string
- */
+    /**
+     * 
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    protected $nome;
 
-protected $nome;
+    public function getId() {
+        return $this->id;
+    }
 
-public function getId() {
-    return $this->id;
-}
+    public function setId($id) {
+        $this->id = $id;
+    }
 
-public function setId($id) {
-    $this->id = $id;
-}
+    public function getNome() {
+        return $this->nome;
+    }
 
-public function getNome() {
-    return $this->nome;
-}
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
 
-public function setNome($nome) {
-    $this->nome = $nome;
-}
+    public function __toString() {
+        return $this->nome;
+    }
 
-public function  __toString(){
-    return $this->nome;
-}
- 
-public function toArray(){
-    return array('id'=>$this->getId(),'nome'=>$this->getNome());
-}
+    public function toArray() {
+        return array(
+            'id' => $this->getId(),
+            'nome' => $this->getNome()
+        );
+    }
 
 }
