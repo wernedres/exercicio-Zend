d268824fb82a8092eaacf753d67d45c0c309d314
Wernedres <wernedres@gmail.com>
2014-08-18 11:59:51 -0400

    [ADJUST]inserido class AbstractService no projeto
    Signed-off-by: Wernedres <wernedres@gmail.com>

--- module/Livraria/src/Livraria/Service/Categoria.php
+++ module/Livraria/src/Livraria/Service/Categoria.php
@@ -6,40 +6,12 @@ use Doctrine\ORM\EntityManager;
 use Livraria\Entity\Categoria as CategoriaService;
 use Livraria\Entity\Configurator;
 
-class Categoria {
-
-    /**
-     * @var EntityManager
-     */
-    protected $em;
-
+class Categoria extends AbstractService {
+    
     public function __construct(EntityManager $em) {
-        $this->em = $em;
+        parent::__construct($em);
+        $this->entity = 'Livraria\Entity\Categoria';
+        
     }
-
-    public function insert(array $data) {
-        $entity = new CategoriaService($data);
-        $this->em->persist($entity); //fila de acoes do banco de dados
-        $this->em->flush(); // excutando a acao do  persist
-        return $entity;
-    }
-
-    public function update(array $data) {
-        $entity = $this->em->getReference('Livraria\Entity\Categoria', $data['id']);
-        $entity = Configurator::configure($entity, $data);
-
-        $this->em->persist($entity); //fila de acoes do banco de dados
-        $this->em->flush(); // excutando a acao do  persist
-        return $entity;
-    }
-
-    public function delete($id) {
-        $entity = $this->em->getReference('Livraria\Entity\Categoria', $id);
-        if ($entity) {
-            $this->em->remove($entity);
-            $this->em->flush();
-            return $id;
-        }
-    }
-
+    
 }
